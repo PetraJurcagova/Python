ee353fffe250b41ab857972b4945e034887c7045
Petra Jurcagova <jurcagova.p@gmail.com>
2023-05-06 18:58:12 +0200

    Aplikace na generovani vtipnych jmen

---
+++ aplikace_na_vtipna_jmena.py
@@ -0,0 +1,15 @@
+print("Vitejte v aplikaci na generovani vtipnych jmen")
+
+name = input("Jake je tve krestni jmeno?\n")
+print(name)
+
+vlastnost = input("Jaka je tva typicka vlastnost? Napis ji s velkym pismenem\n")
+
+print(f"Tvoje vtipne jmeno je {name} {vlastnost}.")
+
+
+
+
+
+
+
