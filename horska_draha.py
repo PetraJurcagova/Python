d1a84ac1585d0dcc47595e3ac6fdb089f96d4dbe
Petra Jurcagova <jurcagova.p@gmail.com>
2023-05-08 18:20:05 +0200

    Horska draha_doplneni o foceni

--- horska_draha.py
+++ horska_draha.py
@@ -1,18 +1,27 @@
 print("Vitejte na horske draze!")
 
 height = int(input("Jaka je vase vyska v cm?\n"))
-
+bill = 0
 
 if height >= 87:
     print("Muzete jet na horske draze!")
 
     age = int(input("Kolik je vam let?\n")) # doplnena podminka veku a cena listku
     if age < 12:
+        bill = 50
         print("Cena vaseho listku je 50 kc.")
     elif age < 18:
+        bill = 100
         print("Cena vaseho listku je 100 Kc.")
     else:
+        bill = 150
         print("Cena vaseho listku je 150 Kc.")
+
+    photo = input("Chcete se nechat vyfotit? A nebo N\n")
+    if photo == 'A':
+        #bill = bill + 50
+        bill += 50
+        print(f"Cena vaseho listku je {bill} Kc.")
 else:
     print("Nemuzete jet na horske draze.")
     
