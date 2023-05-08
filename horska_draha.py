48eb1f1f17a79a8c557b9628437d83fcd4e62815
Petra Jurcagova <jurcagova.p@gmail.com>
2023-05-08 17:37:34 +0200

    Horska draha prvni cast

---
+++ horska_draha.py
@@ -0,0 +1,20 @@
+print("Vitejte na horske draze!")
+
+height = int(input("Jaka je vase vyska v cm?\n"))
+
+if height >= 87:
+    print("Muzete jet na horske draze!")
+else:
+    print("Nemuzete jet na horske draze.")
+
+
+
+
+
+
+
+
+
+
+
+
