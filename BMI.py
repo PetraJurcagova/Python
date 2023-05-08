71bbbbdfbd65403e6417c4cd0d5cd4284d54936c
Petra Jurcagova <jurcagova.p@gmail.com>
2023-05-08 16:34:46 +0200

    vypocet BMI indexu zaklad

---
+++ BMI.py
@@ -0,0 +1,18 @@
+
+height = float(input("Zadejte svou vysku v metrech.\n"))
+weight = int(input("Zadejte svou vahu v kg\n"))
+
+
+BMI = weight/(height*height)
+#BMI = int(weight) / (float(height) * float(height))
+
+BMI = int(BMI)
+
+print(f"Vas BMI index je {BMI}.")
+
+
+
+
+
+
+
