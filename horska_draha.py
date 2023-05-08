549458e0b15b4bf9793b84ab2959aaeb1cc7e357
Petra Jurcagova <jurcagova.p@gmail.com>
2023-05-08 17:44:54 +0200

    Doplneni veku a ceny listku

--- horska_draha.py
+++ horska_draha.py
@@ -2,10 +2,18 @@ print("Vitejte na horske draze!")
 
 height = int(input("Jaka je vase vyska v cm?\n"))
 
+
 if height >= 87:
     print("Muzete jet na horske draze!")
+
+    age = int(input("Kolik je vam let?\n")) # doplnena podminka veku a cena listku
+    if age < 18:
+        print("Cena vaseho listku je 100 kc.")
+    else:
+        print("Cena vaseho listku je 150 Kc.")
 else:
     print("Nemuzete jet na horske draze.")
+    
 
 
 
