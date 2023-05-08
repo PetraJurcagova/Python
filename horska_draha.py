2d71e506a352440b2dc7ec8964d6a29b59e2a178
Petra Jurcagova <jurcagova.p@gmail.com>
2023-05-08 17:57:36 +0200

    Horska draha rozsireni veku o elif

--- horska_draha.py
+++ horska_draha.py
@@ -7,8 +7,10 @@ if height >= 87:
     print("Muzete jet na horske draze!")
 
     age = int(input("Kolik je vam let?\n")) # doplnena podminka veku a cena listku
-    if age < 18:
-        print("Cena vaseho listku je 100 kc.")
+    if age < 12:
+        print("Cena vaseho listku je 50 kc.")
+    elif age < 18:
+        print("Cena vaseho listku je 100 Kc.")
     else:
         print("Cena vaseho listku je 150 Kc.")
 else:
