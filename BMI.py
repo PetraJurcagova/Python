19b42bbd539a75c3badb30efcacd4b0c80db3e16
Petra Jurcagova <jurcagova.p@gmail.com>
2023-05-10 12:18:33 +0200

    Vylepseni BMI o podminky

--- BMI.py
+++ BMI.py
@@ -6,9 +6,21 @@ weight = int(input("Zadejte svou vahu v kg\n"))
 BMI = weight/(height*height)
 #BMI = int(weight) / (float(height) * float(height))
 
-BMI = int(BMI)
+#BMI = float(BMI)
+BMI = float(round(BMI, 2))
 
 print(f"Vas BMI index je {BMI}.")
+
+if BMI <+ 18.5:
+    print("Mate podvahu.")
+elif BMI <= 24.9:
+    print("Mate normalni vahu.")
+elif BMI <= 29.9:
+    print("Mate mirnou nadvahu, zkuste cvicit!")
+elif BMI <= 34.9:
+    print("Jste obezni, zacnete se hybat.")
+else:
+    print("Trpite extremni obezitou, vyhledejte sprintem lekare!")
 
 
 
