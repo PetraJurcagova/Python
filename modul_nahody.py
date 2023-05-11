9c634e3947cb05101ea6d4ccd30ede5e9fbdc8a2
Petra Jurcagova <jurcagova.p@gmail.com>
2023-05-11 12:31:39 +0200

    kombinace math a random

--- modul_nahody.py
+++ modul_nahody.py
@@ -1,9 +1,19 @@
 import random
+import math
 # generovani nahodneho celeho cisla z rozsahu
 print(random.randint(10,20))
 
 # generovani nahodneho floatu (desetineho cisla) mezi 0 a 1
 print(random.random())
+print(random.random() * 6) # nahodny float v rozsahu 0 - 6
+
+#simulace hazeni kostkou
+print(math.ceil(random.random()*6))
+print(math.ceil(random.random()*6))
+print(math.ceil(random.random()*6))
+print(math.ceil(random.random()*6))
+
+
 
 # generovani nahodneho celeho cisla v rozsahu zde po dvou krocich
 print(random.randrange(15,25,2))
