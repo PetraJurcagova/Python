db4e087a6d980e711e37c55b11cb2b2ed4345dd1
Petra Jurcagova <jurcagova.p@gmail.com>
2023-05-12 10:31:51 +0200

    kocka a mys 2. cast

--- oznacujeme_pozici.py
+++ oznacujeme_pozici.py
@@ -3,15 +3,24 @@ set_2 = ["😺", "😺", "😺"]
 set_3 = ["😺", "😺", "😺"]
 
 
-set_4 = ["🐭"]
+x = "🐭"
 
 all_sets = [set_1, set_2, set_3]
-print(all_sets)
 
+#print(all_sets)
+print(f"{set_1}\n{set_2}\n{set_3}")
 
+position = input("Zadejte souradnice:\n") #  protoze se jedna o string, tak muzeme jednotlive prvky 'vyndat' a pak je prevest na int
 
+# vypis znaku mysi
+#rozdeleni souradnic na cisla
+num_1 = int(position[0])
+num_2 = int(position[1])
+#print(num_1)
+#print(num_2)
 
-
+all_sets[num_1][num_2] = x
+print(f"{set_1}\n{set_2}\n{set_3}")
 
 
 
