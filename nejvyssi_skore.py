b895eded7836e4b04412f77de752e1a0755156fc
Petra Jurcagova <jurcagova.p@gmail.com>
2023-05-15 11:15:19 +0200

    minimum a maximum bez funkce

--- nejvyssi_skore.py
+++ nejvyssi_skore.py
@@ -8,7 +8,7 @@ score = input("Zadejte skore jednotlivych studentu oddelene carkou a mezerou:\n"
 score_list = score.split(", ")
 print(score_list)
 
-
+# prevedeni na cisla
 score_list_number = []
 
 for index in range(0, len(score_list)):
@@ -16,8 +16,19 @@ for index in range(0, len(score_list)):
     score_list_number.append (int(score_list[index]))
 print(score_list_number)
    
+#maximum
+#maximum = 0
+#for one_number in score_list_number:
+    #if one_number > maximum:
+        #maximum = one_number
+#print(maximum)
 
-
+#minimum
+minimum = 100
+for min_number in score_list_number:
+    if min_number < minimum:
+        minimum = min_number
+print(minimum)
 
 
 
